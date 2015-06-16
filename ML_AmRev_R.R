a2aad0d84a0e229edb64e03329d47078942d0ec7
Apolo Ro <apolitogaga@gmail.com>
2015-06-16 15:24:39 +0200

    test

--- ML_AmRev_R.R
+++ ML_AmRev_R.R
@@ -8,6 +8,5 @@ data2 <- read.arff("data/authorDS/Amazon_initial_50_30_10000.arff")#Amazon_initi
 data3 <- read.arff("data/authorDS/charactersdataset_20_50.arff")
 data4 <- read.arff("data/authorDS/charactersdataset_30_50.arff")
 
-
 ?read.arff
 getwd()
