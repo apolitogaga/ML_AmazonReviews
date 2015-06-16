94dc897d58d3ce3ea40e8378beed6d6eac1f2e49
Apolo Ro <apolitogaga@gmail.com>
2015-06-16 15:18:11 +0200

    Add dartasets we got from the author
    The dataset we got from the author of the UCI dataset.

--- ML_AmRev_R.R
+++ ML_AmRev_R.R
@@ -1,12 +1,9 @@
 install.packages("foreign")
- 
 library(foreign)
-install.packages("RWeka")
-library(RWeka)
 
 read.arff("data/wise2014-train.arff")
 data<- read.arff("data/amazon-commerce-reviews.arff")
 
-data2 <- read.arff("data/amRevData1.arff")#Amazon_initial_50_30_10000
+data2 <- read.arff("data/authorDS/Amazon_initial_50_30_10000.arff")#Amazon_initial_50_30_10000
 ?read.arff
 getwd()
