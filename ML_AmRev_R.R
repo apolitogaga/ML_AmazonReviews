6b380b2c82a1c335386d2dc067942b3bfacb8dd4
Apolo Ro <apolitogaga@gmail.com>
2015-06-16 15:21:51 +0200

    load some shit

--- ML_AmRev_R.R
+++ ML_AmRev_R.R
@@ -5,5 +5,9 @@ read.arff("data/wise2014-train.arff")
 data<- read.arff("data/amazon-commerce-reviews.arff")
 
 data2 <- read.arff("data/authorDS/Amazon_initial_50_30_10000.arff")#Amazon_initial_50_30_10000
+data3 <- read.arff("data/authorDS/charactersdataset_20_50.arff")
+data4 <- read.arff("data/authorDS/charactersdataset_30_50.arff")
+
+
 ?read.arff
 getwd()
