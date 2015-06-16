dc00dbbc4aa17932795668296b5722da19c96a1f
MiguelMossa <mossamiguel@gmail.com>
2015-06-16 17:07:46 +0200

    project ML

--- ML_AmRev_R.R
+++ ML_AmRev_R.R
@@ -3,7 +3,39 @@ library(foreign)
 
 read.arff("data/wise2014-train.arff")
 data<- read.arff("data/amazon-commerce-reviews.arff")
+attributes(data)
+names(data)
+plot(data$Class)
+class<-data[,10001]
+dim(data)
 
-data2 <- read.arff("data/authorDS/Amazon_initial_50_30_10000.arff")#Amazon_initial_50_30_10000
-?read.arff
-getwd()
+v <- sort(colSums(matrixdata),decreasing=TRUE)
+plot(log(v))
+d <- data.frame(word = colnames(data[,-10001]),freq=v/sum(v))
+wordcloud(d$word,d$freq,scale=c(8,.5),max.words=100, random.order=FALSE)
+plot(data[,10000],data[,10001])
+
+library(tm)
+
+matrixdata<-as.matrix(data[-10001])
+# Now we weight by inverse document frequency
+data.tfidf <- tfidf.weight(matrixdata)
+# and normalize by vector length
+data.tfidf <- div.by.euc.length(data.tfidf)
+
+dim(data.tfidf)
+summary(colSums(data.tfidf))
+
+# remove those words whose total sum is not greater than the third quartile of the distribution
+
+(r.3rdQ <- summary(colSums(data.tfidf))[5])
+
+data.tfidf.2 <- subset.data.frame (data.tfidf, 
+                                         select=colSums(data.tfidf)>r.3rdQ)
+dim(data.tfidf.2)
+
+
+
+
+
+
