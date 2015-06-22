92a9d820331255acd12c280a964a1c9e360ae743
Apolo Ro <apolitogaga@gmail.com>
2015-06-22 22:06:44 +0200

    Second second to last push
    Missed something

--- ML_AmRev_R.R
+++ ML_AmRev_R.R
@@ -40,6 +40,8 @@ doTheCloud <- function(data2,rand=FALSE){
   wordcloud(d$word,d$freq,scale=c(8,.3), random.order=rand, color=pal2)
 }
 
+# Please run the functions from the Reuters auxiliary functions which are found at the end of this document
+# We put them there because we din't design them, we just use them.
 
 ################################################################################################################################################
 #---------Load and create test sets-------------------------------------------
@@ -290,14 +292,12 @@ Cs3<-numeric(length(K))
 error.rate.test.Ondim3<-numeric(length(K))
 error.rate.harmonic3<-numeric(length(K))
 error.CV3<-numeric(length(K))
-#SV<-numeric(length(K))
 K=c(1000,1500,2000) # dim size to use CV
 (ntrees <- round(10^seq(1,4,by=0.4))) # ntrees posibilities
 
 
 for(i in 1:length(K)){
   for(j in 1:length(ntrees)){
-    #mi.svm <- ksvm (target.class1~.,data=Psiframe[tr,1:K[i]],C=Cp[j],cross=10)
     mi.rf <- randomForest(target.class1 ~ ., data = Psiframe[tr,1:K[i]], ntree=ntrees[j], proximity=FALSE, strata=x$target.class1)
     err3[j]<- mi.rf$err.rate[ntrees[j],1]
   }
@@ -305,15 +305,10 @@ for(i in 1:length(K)){
   Cs3[i]<-ntrees[nTreemin]
   error.CV3[i]<-min(err3)
   mi.rf.final <- randomForest(target.class1 ~ ., data = Psiframe[tr,1:K[i]], ntree=ntrees[nTreemin], proximity=FALSE, strata=x$target.class1)
-  #mi.svm.final <- ksvm (target.class1~.,data=Psiframe[tr,1:K[i]],C=Cp[Cmin],cross=10)
-  
-  #SV[i]<-nSV(mi.svm.final) # Support vectors
-  
-  #svmpred <- predict (mi.svm.final, Psiframe[-tr,2:K[i]])
+
   pred.rf <- predict (mi.rf.final, Psiframe[-tr,2:K[i]], type="class")
   
   (ct <- table(Truth=datacomplete[-tr,ncol(datacomplete)], Pred=pred.rf))
-  #tt <- table(datacomplete[-tr,ncol(datacomplete)],svmpred)
   
   
   
@@ -341,27 +336,18 @@ for(i in 1:length(K)){
   colnames(x)[1] <- "target.class1"
   #Calculate the best C parameter
   for(j in 1:length(ntrees)){
-    #mi.svm2 <- ksvm (target.class1~.,data=x[tr,],C=Cp[j],cross=10)11
     mi.rf <- randomForest(target.class1 ~ ., data = x[tr,], ntree=ntrees[j], proximity=FALSE, strata=x$target.class1)
     err4[j]<- mi.rf$err.rate[ntrees[j],1]
-    #err2[j]<-cross(mi.svm2)
   }
   nTreemin <- which.min(err4)
   Cs4[i]<-ntrees[nTreemin] # the best C
   error.CV4[i]<-min(err4) # CV error with the best C
   
   mi.rf.final2 <- randomForest(target.class1 ~ ., data = x[tr,], ntree=ntrees[nTreemin], proximity=FALSE, strata=x$target.class1)
-  #mi.svm.final2 <- ksvm (target.class1~.,data=x[tr,],C=Cp[Cmin],cross=10) # recalculate the model using the best C parameter
-  
-  #SV2[i]<-nSV(mi.svm.final2) # Support vectors
   
   pred.rf2 <- predict (mi.rf.final2, x[-tr,], type="class")
   
   (ct <- table(Truth=datacomplete[-tr,ncol(datacomplete)], Pred=pred.rf2))
-  
-  #svmpred2 <- predict (mi.svm.final2, datacomplete[-tr,-ncol(datacomplete)])
-  
-  #tt <- table(datacomplete[-tr,ncol(datacomplete)],svmpred2)
   
   error_rate.test <- 100*(1-sum(diag(ct))/sum(ct))
   print(error_rate.test)
@@ -398,3 +384,159 @@ print(xtable(Testing.PCA.rf))
 
 print(xtable(Testing.Relief.rf))
 print(xtable(Testing.Relief.rf2))
+
+
+################################################################################################################################################
+#---------Functions from REUTERS aux file, from lab 1-bis ------------------------------------------
+################################################################################################################################################
+
+
+
+####################################################################
+# Functions for document retrieval and simple similarity searching
+# Modified from code written by Tom Minka (thanks!)
+####################################################################
+
+# Rescale rows of an array or data frame by a given weight vector
+
+scale.rows <- function(x,w) apply(x,2,function(x){x*w})
+
+# Rescale columns of an array or data frame by a given weight vector
+
+scale.cols <- function(x,w) t(apply(x,1,function(x){x*w}))
+
+
+# Normalize vectors by the sum of their entries
+# Input assumed to be a set of vectors in array form, one vector per row
+
+div.by.sum <- function(x) scale.rows(x,1/(rowSums(x)+1e-16))
+
+# Normalize vectors by their Euclidean length
+# Input assumed to be a set of vectors in array form, one vector per row
+
+div.by.euc.length <- function(x) scale.rows(x,1/sqrt(rowSums(x^2)+1e-16))
+
+
+# Turn a string into a vector of words
+# for comparability across bags of words, also strip out punctuation and
+# numbers, and shift all letters into lower case
+
+strip.text <- function(txt) 
+{
+  # remove apostrophes (so "don't" -> "dont", "Jane's" -> "Janes", etc.)
+  txt <- gsub("'","",txt)
+  # convert to lowercase
+  txt <- tolower(txt)
+  # change other non-alphanumeric characters to spaces
+  txt <- gsub("[^a-z0-9]"," ",txt)
+  # change digits to # and remove them
+  txt <- gsub("[0-9]+","",txt)
+  
+  # split and make one vector
+  txt <- unlist(strsplit(txt," "))
+  # remove empty words
+  txt <- txt[txt != ""]
+  txt
+}
+
+# Remove columns from a ragged array which only appear in one row
+
+remove.singletons.ragged <- function(x) 
+{
+  # Collect all the column names, WITH repetition
+  col.names <- c()
+  for(i in 1:length(x))
+  {
+    col.names <- c(col.names, names(x[[i]]))
+  }
+  # See how often each name appears
+  count <- table(col.names)
+  # Loop over vectors and keep only the columns which show up more than once
+  for(i in 1:length(x)) 
+  {
+    not.single <- (count[names(x[[i]])] > 1)
+    x[[i]] <- x[[i]][not.single]
+  }
+  x
+}
+
+# Standardize a ragged array so all vectors have the same length and ordering
+# Supplies NAs for missing values
+# Input: a list of vectors with named columns
+# Output: a standardized list of vectors with named columns
+
+standardize.ragged <- function(x) 
+{
+  # Keep track of all the column names from all the vectors in a single vector
+  col.names <- c()
+  # Get the union of column names by iterating over the vectors - using
+  # setdiff() is faster than taking unique of the concatenation, the more
+  # obvious approach
+  for(i in 1:length(x)) 
+  {
+    col.names <- c(col.names, setdiff(names(x[[i]]),col.names))
+  }
+  # put the column names in alphabetical order, for greater comprehensibility
+  col.names <- sort(col.names)
+  # Now loop over the vectors again, putting them in order and filling them out
+  # Note: x[[y]] returns NA if y is not the name of a column in x
+  for (i in 1:length(x)) 
+  {
+    x[[i]] <- x[[i]][col.names]
+    # Make sure the names are right
+    names(x[[i]]) <- col.names
+  }
+  x
+}
+
+
+# Turn a list of bag-of-words vectors into a data frame, one row per bag
+# Input: 
+#   list of bag-of-words vectors
+#   list of row names (optional),
+#   flag for whether singletons should be removed,
+#   flag for whether words missing in a document should be coded 0
+# Output: data frame, columns named by the words and rows matching documents
+
+make.BoW.frame <- function(x,row.names,remove.singletons=TRUE,
+                           absent.is.zero=TRUE) 
+{
+  # Should we remove one-time-only words?
+  if (remove.singletons) { y <- remove.singletons.ragged(x) }
+  else { y <- x }
+  
+  # Standardize the column names
+  y <- standardize.ragged(y)
+  
+  # Transform the list into an array (there are probably slicker ways to do this)
+  z = y[[1]] # Start with the first row
+  if (length(y) > 1) { # More than one row?
+    for (i in 2:length(y)) 
+    {
+      z = rbind(z,y[[i]],deparse.level=0) # then stack them
+    }
+  }
+  
+  # Make the data frame and use row names, if provided
+  if (missing(row.names)) { BoW.frame <- data.frame(z) }
+  else { BoW.frame <- data.frame(z,row.names=row.names) }
+  
+  if (absent.is.zero) 
+  {
+    # The standardize.ragged function maps missing words to "NA", so replace
+    # those with zeroes to simplify calculation
+    BoW.frame <- apply(BoW.frame,2,function(q){ifelse(is.na(q),0,q)})
+  }
+  BoW.frame
+}
+
+
+# Compute inverse document frequency weights and rescale a data frame
+
+tfidf.weight <- function(x) 
+{
+  # TFIDF weighting
+  doc.freq <- colSums(x>0)
+  doc.freq[doc.freq == 0] <- 1
+  scale.cols(x, log(nrow(x)/doc.freq))
+}
